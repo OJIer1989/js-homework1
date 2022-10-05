7e65aa0839b6fdc329ac4a7395c7280fda20223e
OJIer1989 <111180353+OJIer1989@users.noreply.github.com>
2022-10-05 09:14:28 +0300

    h-w1

---
+++ homework1/js/main.js
@@ -0,0 +1,45 @@
+let FirstNumber = 14;
+let SecondNumber = 2;
+let ThirdNumber = 3;
+
+
+console.log(FirstNumber+SecondNumber*ThirdNumber);
+console.log(FirstNumber*ThirdNumber/ThirdNumber);
+console.log(FirstNumber-SecondNumber*ThirdNumber);
+
+
+let FirstText = 'Tom';
+let SecondText = 'Sara';
+let LastText = 'Den';
+let AllText = FirstText + SecondText + LastText;
+
+console.log(FirstText+SecondText);
+console.log(AllText+SecondText+FirstNumber);
+
+
+
+let TrueValue = true;
+let FalseValue = false;
+console.log(TrueValue);
+
+let UserName = prompt('User Name');
+let SecondName = prompt('Last Name');
+console.log(UserName);
+document.writeln(UserName);
+console.log(SecondName);
+document.writeln(SecondName);
+
+let LastName = UserName + SecondName;
+console.log(LastName);
+
+
+let NumberOne = 20;
+let NumberTwo = 15;
+
+
+console.log('Ukraine', NumberOne==NumberOne);
+console.log('ru', NumberOne==NumberTwo);
+
+console.log('Ukraine',NumberOne!=NumberTwo )
+console.log('ru',NumberOne!=NumberOne )
+
